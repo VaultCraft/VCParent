14ea385c9612d3d56a95250e3f2c5f0c3ae0269c
Connor Hollasch <connor@hollasch.net>
2014-07-19 23:43:38 -0700

    Initial Commit

---
+++ src/main/java/net/vaultcraft/vcutils/VCUtils.java
@@ -0,0 +1,18 @@
+package net.vaultcraft.vcutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Connor on 7/19/14. Designed for the VCUtils project.
+ */
+
+public class VCUtils extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+        
+    }
+}
