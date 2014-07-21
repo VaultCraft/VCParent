1256f7b23d9b56310d4b0d77b25f3858f322a06a
Nicholas <tacticalsk8er@gmail.com>
2014-07-21 10:58:00 -0700

    Added Feed and God

---
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCFeed.java
@@ -0,0 +1,40 @@
+package net.vaultcraft.vcessentials.commands;
+
+import net.vaultcraft.vcutils.chat.Form;
+import net.vaultcraft.vcutils.chat.Prefix;
+import net.vaultcraft.vcutils.command.ICommand;
+import net.vaultcraft.vcutils.user.Group;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by tacticalsk8er on 7/21/2014.
+ */
+public class VCFeed extends ICommand {
+    public VCFeed(String name, Group permission, String... aliases) {
+        super(name, permission, aliases);
+    }
+
+    @Override
+    public void processCommand(Player player, String[] args) {
+        if(args.length == 0) {
+            player.setFoodLevel(20);
+            player.setSaturation(12.8f);
+            Form.at(player, Prefix.SUCCESS, "Fed.");
+            return;
+        }
+
+        if(args.length == 1) {
+            Player player1 = Bukkit.getPlayer(args[0]);
+            if(player1 == null) {
+                Form.at(player, Prefix.ERROR, "Player: " + args[0] + " is not online.");
+                return;
+            }
+
+            player1.setFoodLevel(20);
+            player1.setSaturation(12.8f);
+            Form.at(player, Prefix.SUCCESS, player1.getName() + " fed.");
+            Form.at(player1, Prefix.SUCCESS, "Fed.");
+        }
+    }
+}
