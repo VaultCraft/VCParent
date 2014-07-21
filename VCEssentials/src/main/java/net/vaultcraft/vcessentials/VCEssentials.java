582ee65f5e9cbe1a6061c58c2603f306a033fa98
Nick Peterson <tacticalsk8er@gmail.com>
2014-07-21 16:56:33 -0700

    Added MySQL tables.

--- VCEssentials/src/main/java/net/vaultcraft/vcessentials/VCEssentials.java
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/VCEssentials.java
@@ -1,7 +1,9 @@
 package net.vaultcraft.vcessentials;
 
 import net.vaultcraft.vcessentials.commands.*;
+import net.vaultcraft.vcutils.VCUtils;
 import net.vaultcraft.vcutils.command.CommandManager;
+import net.vaultcraft.vcutils.database.sql.MySQL;
 import net.vaultcraft.vcutils.user.Group;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,11 +14,50 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class VCEssentials extends JavaPlugin {
 
     private static VCEssentials instance;
+    private MySQL mySQL = VCUtils.getInstance().mySQL;
 
     public void onEnable() {
         instance = this;
 
         initCommands();
+        mySQL.updateThread.add("CREATE TABLE IF NOT EXISTS Bans(" +
+                "ID INT NOT NULL AUTO_INCREMENT," +
+                "PRIMARY KEY(ID)," +
+                "BannedID TINYTEXT," +
+                "BannedName CHAR(16)," +
+                "BannerID TINYTEXT" +
+                "BannerName CHAR(16)," +
+                "Reason TEXT," +
+                "Time DATETIME," +
+                "Temp DATETIME," +
+                "Unbanned BIT(1))");
+        mySQL.updateThread.add("CREATE TABLE IF NOT EXISTS Kicks(" +
+                "ID INT NOT NULL AUTO_INCREMENT," +
+                "PRIMARY KEY(ID)," +
+                "KickedID TINYTEXT," +
+                "KickedName CHAR(16)," +
+                "KickerID TINYTEXT," +
+                "KickerName CHAR(16)," +
+                "Reason TEXT," +
+                "Time DATETIME)");
+        mySQL.updateThread.add("CREATE TABLE IF NOT EXISTS Mutes(" +
+                "ID INT NOT NULL AUTO_INCREMENT," +
+                "PRIMARY KEY(ID)," +
+                "MutedID TINYTEXT," +
+                "MutedName CHAR(16)," +
+                "MuterID TINYTEXT," +
+                "MuterName CHAR(16)," +
+                "Reason TEXT," +
+                "Time DATETIME," +
+                "Temp DATETIME," +
+                "Unmuted BIT(1))");
+        mySQL.updateThread.add("CREATE TABLE IF NOT EXISTS Chat(" +
+                "ID INT NOT NULL AUTO_INCREMENT," +
+                "PRIMARY KEY(ID)," +
+                "ChatterID TINYTEXT," +
+                "ChatterName CHAR(16)," +
+                "Message TEXT," +
+                "Time DATETIME)");
     }
 
     public static VCEssentials getInstance() {
