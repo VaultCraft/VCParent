dfdcef22196212302159a68967df85ca0d4afc94
Nicholas <tacticalsk8er@gmail.com>
2014-07-20 22:55:28 -0700

    Finished MongoDB

--- src/main/java/net/vaultcraft/vcutils/database/mongo/MongoDB.java
+++ src/main/java/net/vaultcraft/vcutils/database/mongo/MongoDB.java
@@ -1,7 +1,147 @@
 package net.vaultcraft.vcutils.database.mongo;
 
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+
 /**
- * Created by tacticalsk8er on 7/20/2014.
+ * WARNING: This class has not been tested!
+ *
+ * @author tacticalsk8er
  */
 public class MongoDB {
+
+    private MongoClient client;
+
+
+    /**
+     * Creates a connection to a MongoDB server and provides easy methods to update and query that server.
+     *
+     * @param host IP of the MongoDB server.
+     * @param port Port number of the MongoDB server.
+     * @throws UnknownHostException
+     */
+    public MongoDB(String host, int port) throws UnknownHostException {
+        client = new MongoClient(host, port);
+    }
+
+    /**
+     *
+     * @return MongoClient
+     */
+    public MongoClient getClient() {
+        return client;
+    }
+
+    /**
+     * Use this to retrieve a DB object and modify it.
+     *
+     * @param dbName Name of database to access
+     */
+    public DB getDB(String dbName) {
+        return client.getDB(dbName);
+    }
+
+    /**
+     * Inserts a BasicDBObject into a DBCollection.
+     *
+     * @param db             DB object to modify.
+     * @param collectionName Name of "table" in SQL terms.
+     * @param dbObject       Object to insert. In SQL terms inserting a "row" into a "table" (collection).
+     */
+    public void update(DB db, String collectionName, BasicDBObject dbObject) {
+        DBCollection dbCollection = db.getCollection(collectionName);
+        dbCollection.insert(dbObject);
+    }
+
+    /**
+     * Inserts a BasicDBObject into a DBCollection.
+     *
+     * @param dbName         The name of the database you want to modify.
+     * @param collectionName Name of "table" in SQL terms.
+     * @param dbObject       Object to insert. In SQL terms inserting a "row" into a "table" (collection).
+     */
+    public void update(String dbName, String collectionName, BasicDBObject dbObject) {
+        DB db = getDB(dbName);
+        update(db, collectionName, dbObject);
+    }
+
+    /**
+     * Queries the MongoDB server and returns a Object.
+     *
+     * @param db             DB object to modify.
+     * @param collectionName Name of "table" in SQL terms.
+     * @param primaryKey     The primary key you want to look for in the collection's documents, or "rows" in SQL terms.
+     * @param ID             What the primary key's value should be.
+     * @param retrieveKey    The key of the object you want to retrieve from the document found if there is one.
+     * @return The object tied to the retrieveKey. If there is no document found to have the primaryKey tied to the ID, then it will return null.
+     */
+    public Object query(DB db, String collectionName, String primaryKey, Object ID, String retrieveKey) {
+        DBCollection dbCollection = db.getCollection(collectionName);
+        BasicDBObject dbObject = new BasicDBObject();
+        dbObject.put(primaryKey, ID);
+        DBCursor dbCursor = dbCollection.find(dbObject);
+        if (dbCursor.hasNext()) {
+            DBObject dbObject1 = dbCursor.next();
+            return dbObject1.get(retrieveKey);
+        }
+        return null;
+    }
+
+    /**
+     * Queries the MongoDB server and returns a Object.
+     *
+     * @param dbName         The name of the database you want to modify.
+     * @param collectionName Name of "table" in SQL terms.
+     * @param primaryKey     The primary key you want to look for in the collection's documents, or "rows" in SQL terms.
+     * @param ID             What the primary key's value should be.
+     * @param retrieveKey    The key of the object you want to retrieve from the document found if there is one.
+     * @return The object tied to the retrieveKey. If there is no document found to have the primaryKey tied to the ID, then it will return null.
+     */
+    public Object query(String dbName, String collectionName, String primaryKey, Object ID, String retrieveKey) {
+        DB db = getDB(dbName);
+        return query(db, collectionName, primaryKey, ID, retrieveKey);
+    }
+
+
+    /**
+     * Queries the MongoDB server and returns a DBObject.
+     *
+     * @param db             DB object to modify.
+     * @param collectionName Name of "table" in SQL terms.
+     * @param primaryKey     The primary key you want to look for in the collection's documents, or "rows" in SQL terms.
+     * @param ID             What the primary key's value should be.
+     * @return The DBObject that has the primaryKey tied to the ID.
+     */
+    public DBObject query(DB db, String collectionName, String primaryKey, Object ID) {
+        DBCollection dbCollection = db.getCollection(collectionName);
+        BasicDBObject dbObject = new BasicDBObject();
+        dbObject.put(primaryKey, ID);
+        DBCursor dbCursor = dbCollection.find(dbObject);
+        if (dbCursor.hasNext()) {
+            return dbCursor.next();
+        }
+        return null;
+    }
+
+    /**
+     * Queries the MongoDB server and returns a DBObject.
+     *
+     * @param dbName         The name of the database you want to modify.
+     * @param collectionName Name of "table" in SQL terms.
+     * @param primaryKey     The primary key you want to look for in the collection's documents, or "rows" in SQL terms.
+     * @param ID             What the primary key's value should be.
+     * @return The DBObject that has the primaryKey tied to the ID.
+     */
+    public DBObject query(String dbName, String collectionName, String primaryKey, Object ID) {
+        DB db = getDB(dbName);
+        return query(db, collectionName, primaryKey, ID);
+    }
+
+    /**
+     * Closes the connection to the MongoDB server.
+     */
+    public void close() {
+        client.close();
+    }
 }
