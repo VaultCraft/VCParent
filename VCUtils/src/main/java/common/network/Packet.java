dc86b48fa9dce38212c7f41dc8050acfb2c15d68
Connor Hollasch <connor@hollasch.net>
2014-08-21 13:19:29 -0700

    NICK - PUSH YOUR CODE >:(

--- VCUtils/src/main/java/common/network/Packet.java
+++ VCUtils/src/main/java/common/network/Packet.java
@@ -1,12 +1,15 @@
 package common.network;
 
+import java.io.Serializable;
 import java.net.Socket;
 
 /**
  * Created by tacticalsk8er on 8/18/2014.
  */
-public interface Packet<T>{
+public interface Packet<T> extends Serializable {
+
     public T getType();
 
     public void run(Socket socket, String clientName);
+
 }
