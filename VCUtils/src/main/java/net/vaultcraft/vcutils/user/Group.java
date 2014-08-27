1da51acf056fc6e3ebb2e19ad5f5c5eea2daf463
Connor Hollasch <connor@hollasch.net>
2014-08-27 13:54:48 -0700

    Poosh

--- VCUtils/src/main/java/net/vaultcraft/vcutils/user/Group.java
+++ VCUtils/src/main/java/net/vaultcraft/vcutils/user/Group.java
@@ -5,22 +5,26 @@ package net.vaultcraft.vcutils.user;
  */
 public enum Group {
 
-    OWNER("&f/&c&lOWNER&f/ &c%user%&f: &c%message%", 16, false),
-    DEVELOPER("&f/&6&lDEV&f/ &7%user%&6: &f%message%", 15, false),
-    MANAGER("&f/&6&lMANAGER&f/ &e%user%&6: &f%message%", 14, false),
-    ADMIN("&f/&b&lADMIN&f/ &7%user%&b: &f%message%", 13, false),
-    MOD("&f/&3&lMOD&f/ &7%user%&3: &f%message%", 12, false),
-    HELPER("&f/&9&lHELPER&f/ &7%user%&9: &f%message%", 11, false),
-    ENDERDRAGON("&f/&5&lENDER&7&lDRAGON&f/ &5%user%&7: &f%message%", 10, 54, true),
-    ENDERMAN("&f/&5&lENDERMAN&f/ &7%user%&5: &f%message%", 9, 35, true),
-    CREEPER("&f/&2&lCREEPER&f/ &7%user%&2: &f%message%", 8, 27, true),
-    YOUTUBE("&f/&c&lYOU&f&lTUBE&f/ &7%user%&c: &7%message%", 7, true),
-    SKELETON("&f/&lSKELETON&f/ &7%user%&f: &7%message%", 6, 20, true),
-    PIGMAN("&f/&4&lPIGMAN&f/ &7%user%&4: &7%message%", 5, 15, true),
-    SLIME("&f/&a&lSLIME&f/ &7%user%&a: &7%message%", 4, 10, true),
-    WOLF("&f/&8&lWOLF&f/ &7%user%&8: &7%message%", 3, 6, true),
-    SILVERFISH("&f/&7&lSILVERFISH&f/ &7%user%: %message%", 2, 3, true),
-    COMMON("&7%user%&f: &7%message%", 1, 1, false);
+    //STAFF RANKS
+    OWNER("&f/&c&lOWNER&f/ &c%user%&f: &c%message%", 13, false),
+    DEVELOPER("&f/&6&lDEV&f/ &7%user%&6: &f%message%", 12, false),
+    MANAGER("&f/&6&lMANAGER&f/ &e%user%&6: &f%message%", 11, false),
+    ADMIN("&f/&b&lADMIN&f/ &7%user%&b: &f%message%", 10, false),
+    MOD("&f/&3&lMOD&f/ &7%user%&3: &f%message%", 9, false),
+    HELPER("&f/&9&lHELPER&f/ &7%user%&9: &f%message%", 8, false),
+
+    //DONOR RANKS
+    ENDERDRAGON("&f/&5&lENDER&7&lDRAGON&f/ &5%user%&7: &f%message%", 7, 54, true),
+    ENDERMAN("&f/&5&lENDERMAN&f/ &7%user%&5: &f%message%", 6, 40, true),
+    SKELETON("&f/&lSKELETON&f/ &7%user%&f: &7%message%", 4, 25, true),
+    SLIME("&f/&a&lSLIME&f/ &7%user%&a: &7%message%", 3, 14, true),
+    WOLF("&f/&8&lWOLF&f/ &7%user%&8: &7%message%", 2, 6, true),
+
+    //DEFAULT RANK
+    COMMON("&7%user%&f: &7%message%", 1, 1, false),
+
+    //EXTRA
+    YOUTUBE("&f/&c&lYOU&f&lTUBE&f/ &7%user%&c: &7%message%", 5, 20, true);
 
     private String tag;
     private int permLevel;
@@ -53,7 +57,7 @@ public enum Group {
         int level = other.permLevel;
         boolean donor = other.isDonorRank;
 
-        if (this.getPermLevel() >= 13)
+        if (this.getPermLevel() >= 10)
             return true;
 
         if (donor) {
