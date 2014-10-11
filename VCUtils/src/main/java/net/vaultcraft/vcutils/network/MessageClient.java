7b0266a7a1bf06c1e195e6d5a384858056b48540
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-10 17:33:12 -0700

    Autoreconnect on Message server loss

--- VCUtils/src/main/java/net/vaultcraft/vcutils/network/MessageClient.java
+++ VCUtils/src/main/java/net/vaultcraft/vcutils/network/MessageClient.java
@@ -2,10 +2,7 @@ package net.vaultcraft.vcutils.network;
 
 import common.network.Packet;
 import net.minecraft.util.io.netty.bootstrap.Bootstrap;
-import net.minecraft.util.io.netty.channel.Channel;
-import net.minecraft.util.io.netty.channel.ChannelInitializer;
-import net.minecraft.util.io.netty.channel.ChannelOption;
-import net.minecraft.util.io.netty.channel.EventLoopGroup;
+import net.minecraft.util.io.netty.channel.*;
 import net.minecraft.util.io.netty.channel.nio.NioEventLoopGroup;
 import net.minecraft.util.io.netty.channel.socket.SocketChannel;
 import net.minecraft.util.io.netty.channel.socket.nio.NioSocketChannel;
@@ -14,6 +11,8 @@ import net.minecraft.util.io.netty.handler.codec.serialization.ObjectDecoder;
 import net.minecraft.util.io.netty.handler.codec.serialization.ObjectEncoder;
 import net.vaultcraft.vcutils.VCUtils;
 import net.vaultcraft.vcutils.logging.Logger;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by tacticalsk8er on 8/27/2014.
@@ -30,7 +29,7 @@ public class MessageClient {
         this.port = port;
     }
 
-    public MessageClient init() throws Exception {
+    public MessageClient init(){
         workerGroup = new NioEventLoopGroup();
 
         Bootstrap b = new Bootstrap();
@@ -44,10 +43,15 @@ public class MessageClient {
                 })
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.TCP_NODELAY, true);
-        serverChannel = b.connect(host, port).awaitUninterruptibly().channel();
-        Logger.log(VCUtils.getInstance(), "Connected to Message Server.");
-        return this;
-    }
+        serverChannel = b.connect(host, port).addListener((ChannelFuture f) -> {
+            Logger.log(VCUtils.getInstance(), "Lost connection to message server.");
+            if (!f.isSuccess()) {
+                f.channel().eventLoop().schedule((Runnable) this::init, 5, TimeUnit.SECONDS);
+                }
+            }).awaitUninterruptibly().channel();
+            Logger.log(VCUtils.getInstance(), "Connected to Message Server.");
+            return this;
+        }
 
     public static void sendPacket(Packet packet) {
         serverChannel.writeAndFlush(packet);
