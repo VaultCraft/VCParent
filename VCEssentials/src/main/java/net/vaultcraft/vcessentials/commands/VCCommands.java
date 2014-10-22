4b37ba188d944ba2b7dc3daafc40e50220ca6d2e
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-22 01:58:52 -0700

    Try this...

--- VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCCommands.java
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCCommands.java
@@ -9,7 +9,9 @@ import net.vaultcraft.vcutils.user.User;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class VCCommands extends ICommand {
     public VCCommands(String name, Group permission, String... aliases) {
@@ -19,28 +21,26 @@ public class VCCommands extends ICommand {
     @Override
     public void processCommand(Player player, String[] args) {
         HashMap<Group, StringBuilder> commandLists = new HashMap<>();
-        HashMap<Group, Integer> commandCount = new HashMap<>();
+        List<Group> commandGroups = new ArrayList<>();
 
         for(Group g : Group.values()) {
             if(User.fromPlayer(player).getGroup().hasPermission(g)) {
                 StringBuilder sb = new StringBuilder();
                 sb.append(g.getChatColor()).append(ChatColor.BOLD.toString()).append(g.getName()).append(": ").append(ChatColor.RESET).append(g.getChatColor());
                 commandLists.put(g, sb);
-                commandCount.put(g, 0);
             }
         }
         for(ICommand command : CommandManager.getCommands().values()) {
             if(commandLists.keySet().contains(command.gerPermission())) {
                 commandLists.get(command.gerPermission()).append(command.getName()).append(", ");
-                commandCount.put(command.gerPermission(), commandCount.get(command.gerPermission()) + 1);
+                if(!commandGroups.contains(command.gerPermission())) {
+                    commandGroups.add(command.gerPermission());
+                }
             }
         }
 
         Form.at(player, Prefix.VAULT_CRAFT, "You have access to the following commands: ");
-        for(Group g : commandLists.keySet()) {
-            if(commandCount.get(g) <= 0) {
-                continue;
-            }
+        for(Group g : commandGroups) {
             StringBuilder theString = commandLists.get(g);
             if(theString.toString().contains(", ")) {
                 theString.deleteCharAt(theString.lastIndexOf(", "));
