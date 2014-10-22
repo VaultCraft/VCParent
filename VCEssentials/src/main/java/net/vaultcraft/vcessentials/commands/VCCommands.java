5124b545b1064f689234ebdfdaa0be18d145b1da
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-22 01:43:03 -0700

    This should work

--- VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCCommands.java
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCCommands.java
@@ -6,7 +6,10 @@ import net.vaultcraft.vcutils.command.CommandManager;
 import net.vaultcraft.vcutils.command.ICommand;
 import net.vaultcraft.vcutils.user.Group;
 import net.vaultcraft.vcutils.user.User;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
 
 public class VCCommands extends ICommand {
     public VCCommands(String name, Group permission, String... aliases) {
@@ -15,15 +18,26 @@ public class VCCommands extends ICommand {
 
     @Override
     public void processCommand(Player player, String[] args) {
-        StringBuilder sb = new StringBuilder();
-        for(ICommand command : CommandManager.getCommands().values()) {
-            if(command.checkPerms(User.fromPlayer(player))) {
-                sb.append(command.gerPermission().getChatColor().toString()).append(command.getName()).append(", ").append(Prefix.VAULT_CRAFT.getChatColor());
+        HashMap<Group, StringBuilder> commandLists = new HashMap<>();
+
+        for(Group g : Group.values()) {
+            if(User.fromPlayer(player).getGroup().hasPermission(g)) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(g.getChatColor()).append(ChatColor.BOLD.toString()).append(g.getName()).append(": ");
+                commandLists.put(g, sb);
             }
         }
-        if(sb.length() > 0) {
-            sb.deleteCharAt(sb.lastIndexOf(", "));
+        for(ICommand command : CommandManager.getCommands().values()) {
+            if(commandLists.keySet().contains(command.gerPermission())) {
+                commandLists.get(command.gerPermission()).append(command.getName()).append(", ");
+            }
         }
-        Form.at(player, Prefix.VAULT_CRAFT, "You have access to the following commands: " + sb.toString());
+
+        Form.at(player, Prefix.VAULT_CRAFT, "You have access to the following commands: ");
+        for(Group g : commandLists.keySet()) {
+            StringBuilder theString = commandLists.get(g);
+            theString.deleteCharAt(theString.lastIndexOf(", "));
+            player.sendMessage(theString.toString());
+        }
     }
 }
