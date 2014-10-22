e684d666b8a4cb64c95bec142205860a3c7c27c1
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-22 01:52:21 -0700

    Fixups

--- VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCCommands.java
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/commands/VCCommands.java
@@ -19,26 +19,36 @@ public class VCCommands extends ICommand {
     @Override
     public void processCommand(Player player, String[] args) {
         HashMap<Group, StringBuilder> commandLists = new HashMap<>();
+        HashMap<Group, Integer> commandCount = new HashMap<>();
 
         for(Group g : Group.values()) {
             if(User.fromPlayer(player).getGroup().hasPermission(g)) {
                 StringBuilder sb = new StringBuilder();
                 sb.append(g.getChatColor()).append(ChatColor.BOLD.toString()).append(g.getName()).append(": ").append(ChatColor.RESET).append(g.getChatColor());
                 commandLists.put(g, sb);
+                commandCount.put(g, 0);
             }
         }
         for(ICommand command : CommandManager.getCommands().values()) {
             if(commandLists.keySet().contains(command.gerPermission())) {
+                if(commandLists.get(command.gerPermission()).toString().contains(command.getName() + ", ")) {
+                    continue;
+                }
                 commandLists.get(command.gerPermission()).append(command.getName()).append(", ");
+                commandCount.put(command.gerPermission(), commandCount.get(command.gerPermission()) + 1);
             }
         }
 
         Form.at(player, Prefix.VAULT_CRAFT, "You have access to the following commands: ");
         for(Group g : commandLists.keySet()) {
+            if(commandCount.get(g) <= 0) {
+                continue;
+            }
             StringBuilder theString = commandLists.get(g);
             if(theString.toString().contains(", ")) {
                 theString.deleteCharAt(theString.lastIndexOf(", "));
             }
+            if(theString.toString().endsWith(": "))
             player.sendMessage(theString.toString());
         }
     }
