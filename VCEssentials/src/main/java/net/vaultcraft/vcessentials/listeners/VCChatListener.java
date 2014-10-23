d95cdad26ddfd7eb6e97a4b617d0da40c2b65408
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-23 16:44:49 -0700

    Fix ip address bug for chat

--- VCEssentials/src/main/java/net/vaultcraft/vcessentials/listeners/VCChatListener.java
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/listeners/VCChatListener.java
@@ -62,7 +62,7 @@ public class VCChatListener implements Listener {
         }
 
         // Do anti-advertising stuff here
-        if(event.getMessage().replace(" ", "").replaceAll("[^0-9\\^.]", "").matches("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")) {
+        if(event.getMessage().replace(" ", "").replaceAll("[^0-9\\^.]", "").matches("^.*(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).*$")) {
             Form.at(event.getPlayer(), Prefix.WARNING, "Please do not put IP addresses in chat!");
             event.setCancelled(true);
             return;
