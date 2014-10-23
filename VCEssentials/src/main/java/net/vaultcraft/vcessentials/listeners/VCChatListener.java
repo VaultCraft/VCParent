766101390729b62d443acf4e6b3bade9ac5b9e83
Sean McClenaghan <cyberkitsune09@gmail.com>
2014-10-22 17:13:11 -0700

    Get chat working with prison

--- VCEssentials/src/main/java/net/vaultcraft/vcessentials/listeners/VCChatListener.java
+++ VCEssentials/src/main/java/net/vaultcraft/vcessentials/listeners/VCChatListener.java
@@ -9,6 +9,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
@@ -27,7 +28,7 @@ public class VCChatListener implements Listener {
         instance = this;
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         for(Player p : Bukkit.getOnlinePlayers()) {
             if(event.getPlayer() == p) {
